4b886500de4b40a739fb7303a292b7a240b115db
Marcelo Antonio Pereira Marcolino <marcelo.marcolino.dev@gmil.com>
2025-05-17 02:50:44 -0300

    Separar duas sentenças com ponto e vírgula

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ const soma = numero1 + numero2;
 const mensagem = `A soma de ${numero1} e ${numero2} é ${soma}.`;
 
 console.log(mensagem);
+
+// Separar duas sentenças com ponto e vírgula
+const duasSentencas = "Criar o que não existe ainda deve ser a pretensão de todo sujeito que está vivo; A tarefa mais importante de uma pessoa que vem ao mundo é criar algo";
+const frasesSeparadas = duasSentencas.split(';');
+
+console.log(frasesSeparadas);
